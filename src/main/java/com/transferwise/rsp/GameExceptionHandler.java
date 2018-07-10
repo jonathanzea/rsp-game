75a4f9ea574cae63158e5b4c8426fae664425ccd
unknown <jonathanzea91@gmail.com>
2018-07-10 20:52:09 -0300

    initial

---
+++ src/main/java/com/transferwise/rsp/GameExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.transferwise.rsp;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@ControllerAdvice
+public class GameExceptionHandler {
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMismatch() {
+        return "Tool not recognized.\n" +
+                "Possible Tools:\n" +
+                "- ROCK\n" +
+                "- PAPER\n" +
+                "- SCISSORS\n";
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMissingParameter() {
+        return "Please add all the request parameters.\n" +
+                "- name\n" +
+                "- tool\n" +
+                "EG: /register_player?name=Edward&tool=SCISSORS";
+    }
+}
---
+++ src/main/java/com/transferwise/rsp/Player.java
@@ -0,0 +1,44 @@
+package com.transferwise.rsp;
+
+public class Player {
+    private String name;
+    private ToolType tool;
+    private int wins = 0 ;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public Player(String name, ToolType tool) {
+        this.name = name;
+        this.tool = tool;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ToolType getTool() {
+        return tool;
+    }
+
+    public void setTool(ToolType tool) {
+        this.tool = tool;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public void addPoint() {
+        this.wins++;
+    }
+}
